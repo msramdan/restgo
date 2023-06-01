d6f4740a92cef093d8e3a6661db35ca09fa6a2b2
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2023-06-01 15:27:53 +0700

    feat : add validator

--- handler/user.handler.go
+++ handler/user.handler.go
@@ -5,8 +5,15 @@ import (
 	"rest-go/model/entity"
 	"rest-go/model/request"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
+
+type ErrorResponse struct {
+	FailedField string
+	Tag         string
+	Value       string
+}
 
 func UserHandlerGetAll(ctx *fiber.Ctx) error {
 	var users []entity.User
@@ -18,11 +25,21 @@ func UserHandlerGetAll(ctx *fiber.Ctx) error {
 	return ctx.JSON(users)
 }
 func UserHandlerCreate(ctx *fiber.Ctx) error {
-
+	// get dt raw body
 	p := new(request.UserCreateRequest)
 	if err := ctx.BodyParser(p); err != nil {
 		return err
 	}
+	// validator
+	errors := ValidateStruct(*p)
+	if errors != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "failed",
+			"data":    errors,
+		})
+
+	}
+
 	newUser := entity.User{
 		Name:    p.Name,
 		Email:   p.Email,
@@ -32,11 +49,28 @@ func UserHandlerCreate(ctx *fiber.Ctx) error {
 	result := database.DB.Debug().Create(&newUser)
 	if result.Error != nil {
 		return ctx.JSON(fiber.Map{
-			"message": "Create User Failed",
+			"message": "failed",
 		})
 	}
 	return ctx.JSON(fiber.Map{
-		"message": "Create User Success",
+		"message": "success",
 		"data":    newUser,
 	})
+}
+
+var validate = validator.New()
+
+func ValidateStruct(p request.UserCreateRequest) []*ErrorResponse {
+	var errors []*ErrorResponse
+	err := validate.Struct(p)
+	if err != nil {
+		for _, err := range err.(validator.ValidationErrors) {
+			var element ErrorResponse
+			element.FailedField = err.StructNamespace()
+			element.Tag = err.Tag()
+			element.Value = err.Param()
+			errors = append(errors, &element)
+		}
+	}
+	return errors
 }
